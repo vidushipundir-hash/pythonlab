31661178defd2d0e12c3de6fa3d8b71f3798f91a
vidushipundir-hash <vidushipundir341@gmail.com>
2026-02-23 11:21:30 +0530

    output_salary output.py
    HRA = 5000.0
    Total Monthly Salary = 35000.0

---
+++ salary calculation.py
@@ -0,0 +1,15 @@
+# Salary Calculation
+
+basic_salary = 25000
+hra_percent = 20
+bonus = 5000
+
+# Calculate HRA
+hra = (basic_salary * hra_percent) / 100
+
+# Calculate Total Salary
+total_salary = basic_salary + hra + bonus
+
+# Display Results
+print("HRA =", hra)
+print("Total Monthly Salary =", total_salary)
